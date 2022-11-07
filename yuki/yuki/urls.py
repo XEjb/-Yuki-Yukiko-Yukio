47eb48c2cae807fe1a3d21d4d7070ff3c7da22f4
Xejb <vladiq-artamonenko@rambler.ru>
2022-11-07 15:45:49 +0300

    mtv

--- yuki/yuki/urls.py
+++ yuki/yuki/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from yuki.yukiko.views import index
+from django.urls import path, include
+from yukiko.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('yukiko/', index)
+    path('yukiko/', include('yukiko.urls'))
 ]
