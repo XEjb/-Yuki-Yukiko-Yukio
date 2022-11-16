b53e7ea2a94e2e698c690363730adc438d3ca9b7
Xejb <vladiq-artamonenko@rambler.ru>
2022-11-16 15:14:53 +0300

    migrations

--- yuki/yuki/settings.py
+++ yuki/yuki/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5)vhz+(b+ibo5jutol8*&ge$5oxl5z7mh0ll97mcn8)5vyn$2o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -122,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- yuki/yuki/urls.py
+++ yuki/yuki/urls.py
@@ -1,4 +1,5 @@
-
+from django.conf.urls.static import static
+from yuki import settings
 from django.contrib import admin
 from django.urls import path, include
 from yukiko.views import *
@@ -7,5 +8,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('yukiko.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = pageNotFound
