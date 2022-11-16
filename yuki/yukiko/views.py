75d96663d32a085b706796b3b422d210b2db6dad
Xejb <vladiq-artamonenko@rambler.ru>
2022-11-16 17:24:49 +0300

    modification

--- yuki/yukiko/views.py
+++ yuki/yukiko/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseNotFound, Http404
+from .models import *
 
 menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
 
 
 def index(request):
-    return render(request, 'yukiko/index.html', {'menu': menu, 'title': 'Главная страница'})
+    posts = Yukiko.objects.all()
+    return render(request, 'yukiko/index.html', {'posts': posts, 'menu': menu, 'title': 'Главная страница'})
 
 
 def about(request):
-    return render(request, 'yukiko/about.html', {'title': 'О сайте'})
+    return render(request, 'yukiko/about.html', {'menu': menu, 'title': 'О сайте'})
 
 
 def categories(request, yukio):
