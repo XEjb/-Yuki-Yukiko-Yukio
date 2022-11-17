5d4f4b084c4ad56ed3494f1839484a8da03e4db9
Xejb <vladiq-artamonenko@rambler.ru>
2022-11-17 16:30:45 +0300

    urls

--- yuki/yukiko/urls.py
+++ yuki/yukiko/urls.py
@@ -3,5 +3,8 @@ from yukiko.views import *
 
 urlpatterns = [
     path('', index, name='home'),
-    path('about/', about, name='about')
+    path('about/', about, name='about'),
+    path('addpage/', addpage, name='add_page'),
+    path('addpage/', contact, name='contact'),
+    path('addpage/', login, name='login'),
 ]
--- yuki/yukiko/views.py
+++ yuki/yukiko/views.py
@@ -2,29 +2,35 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from .models import *
 
-menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+menu = [{'title': " Инфа", 'url_name': 'about'},
+        {'title': " Авторизация", 'url_name': 'add_page'},
+        {'title': " Фидбэк", 'url_name': 'contact'},
+        {'title': " Вход", 'url_name': 'login'}
+]
 
 
 def index(request):
     posts = Yukiko.objects.all()
-    return render(request, 'yukiko/index.html', {'posts': posts, 'menu': menu, 'title': 'Главная страница'})
+    context = {
+        'posts': posts,
+        'menu': menu,
+        'title': 'Главная страница'
+    }
+    return render(request, 'yukiko/index.html', context=context)
 
 
 def about(request):
     return render(request, 'yukiko/about.html', {'menu': menu, 'title': 'О сайте'})
 
 
-def categories(request, yukio):
-    if request.POST:
-        print(request.POST)
+def addpage(request):
+    return HttpResponse('Авторизация')
 
-    return HttpResponse(f'<h1>Статьи по категориям</h1><p>{yukio}</p>')
+def contact(request):
+    return HttpResponse("Фидбэк")
 
-
-def archive(request, year):
-    if int(year) > 2022:
-        return redirect('home', permanent=True)
-    return HttpResponse(f'<h1>Архив по годам</h1><p>{year}</p>')
+def login(request):
+    return HttpResponse('Вход')
 
 
 def pageNotFound(request, exception):
